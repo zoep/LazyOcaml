d42cde3362e3d7a77182c8a140ae86c03dfe5acf
Zoe P <zoe.paraskevopoulou@gmail.com>
2013-05-03 18:52:12 +0300

    fixed indent

--- test.ml
+++ test.ml
@@ -2,5 +2,5 @@ let x = ref 0 in
 let y = delay (x := !x + 2; !x) in
 let f x = ((force x) + (force x))*5 + 1 in 
 let _ = delay (x := 100) in
-Printf.printf "%d\n" ((f y)*(!x)); (* Yes the answer is zero *)
-Printf.printf "%d\n" ((f y)*(!x))  (* Now the answer is 42. Magic! *)
+  Printf.printf "%d\n" ((f y)*(!x)); (* Yes the answer is zero *)
+  Printf.printf "%d\n" ((f y)*(!x))  (* Now the answer is 42. Magic! *)
